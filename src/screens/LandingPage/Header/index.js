e75dd20b887db481a823435caae317775364970f
Hammad Khalid <hammad.khalid@gigalabs.co>
2021-10-01 18:38:58 +0500

    change the header text css

--- src/screens/LandingPage/Header/index.js
+++ src/screens/LandingPage/Header/index.js
@@ -5,7 +5,9 @@ const LandingPage = () => {
   return (
     <div className={styles.header}>
       <h1 className={styles.mainHeading}>Betting Reimagined</h1>
-      <h6 className={styles.subHeading}>Clear, Social & Fair</h6>
+      <div className={styles.slogan}>
+        <span>Clear, Social & Fair</span>
+      </div>
       <div className={styles.video}>
         <VideoPlayer
           video="https://files.wallfair.io/wallfair.mp4"
