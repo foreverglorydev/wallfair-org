cac996a6e496332cfdeefc64662b3aae0fc5bb78
Hammad Khalid <hammad.khalid@gigalabs.co>
2021-10-25 15:46:31 +0500

    Adding env variables

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -9,19 +9,22 @@ const { appendRoutes } = require('./server/meta');
 const apiPath =
   process.env.REACT_APP_BACKEND_URL || 'https://staging-api.wallfair.io/';
 
-const siteURL = process.env.REACT_APP_URL || 'http://main.wallfair.io/';
+const siteURL = process.env.REACT_APP_URL || 'http://play.wallfair.io/';
+
+const enableSitemap = process.env.ENABLE_SITE_MAP || 0;
 
 const listPaths = ['api/event/list'];
 
 function generateSitemap () {
-  appendRoutes(apiPath, listPaths).then(meta => {
-    const paths = Object.keys(meta);
-    const sitemap = sitemapBuilder(siteURL, paths);
-    fs.writeFile('./public/sitemap.xml', sitemap.toXML(), function (err) {
-      if (err) throw err;
-      console.log('Sitemap generated!');
-    });
-  });
+  enableSitemap ?
+    appendRoutes(apiPath, listPaths).then(meta => {
+      const paths = Object.keys(meta);
+      const sitemap = sitemapBuilder(siteURL, paths);
+      fs.writeFile('./public/sitemap.xml', sitemap.toXML(), function (err) {
+        if (err) throw err;
+        console.log('Sitemap generated!');
+      });
+    }) : console.log('Sitemap not required!');
 }
 
 generateSitemap();
